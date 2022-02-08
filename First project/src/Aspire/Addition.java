86c89c4d2ac9c8f620826c876868bc6a9ef61d28
ELCOT <ELCOT@Lenovo>
2022-02-08 12:22:33 +0530

    Adding- success

---
+++ First project/src/Aspire/Addition.java
@@ -0,0 +1,17 @@
+package Aspire;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter two numbers");
+		int num1 = 12;
+		int num2 =23;
+		System.out.println(num1 +""+ num2);
+		int sum = num1 + num2;
+		System.out.println("The sum is:"+sum);
+		
+
+	}
+
+}
