5ed42fd08c17100190ec19e304007d8c9d7dc2ea
ELCOT <ELCOT@Lenovo>
2022-02-08 12:29:06 +0530

    Merge branch 'main' of https://github.com/Moi-ToucheDor/shams.git into main
    print the integer not success

---
+++ First project/src/Aspire/PrintTheInteger.java
@@ -0,0 +1,14 @@
+package Aspire;
+
+public class PrintTheInteger {
+import java.util.Scanner;
+	 {
+		// creates a reader instance which takes input from system Keyboard
+		Scanner reader = new Scanner(System.in);
+		System.out.println();("Enter a number: ");
+		int number = reader.nextInt();
+		System.out.println("You entered:"+number);
+
+	
+
+
